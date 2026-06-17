lycodump/device_xiaomi_miuicamera-sapphire

Switching to Garnet
Signed-off-by: Kleidione Freitas <kleidione@gmail.com>

BoardConfig.mk
@@ -5,9 +5,9 @@
 #
 
 # Inherit from the proprietary version
-include vendor/xiaomi/miuicamera-cupid/BoardConfigVendor.mk
+include vendor/xiaomi/miuicamera-garnet/BoardConfigVendor.mk
 
-MIUICAMERA_PATH := device/xiaomi/miuicamera-cupid
+MIUICAMERA_PATH := device/xiaomi/miuicamera-garnet
 
 # Properties
 TARGET_SYSTEM_PROP += $(MIUICAMERA_PATH)/system.prop

device.mk
@@ -5,7 +5,7 @@
 #
 
 # Inherit from the proprietary version
-$(call inherit-product, vendor/xiaomi/miuicamera-cupid/miuicamera-cupid-vendor.mk)
+$(call inherit-product, vendor/xiaomi/miuicamera-garnet/miuicamera-garnet-vendor.mk)
 
 # Public libraries
 PRODUCT_COPY_FILES += \

extract-files.py
@@ -18,7 +18,7 @@ from extract_utils.main import (
 )
 
 namespace_imports = [
-    'device/xiaomi/miuicamera-cupid',
+    'device/xiaomi/miuicamera-garnet',
 ]
 
 
@@ -44,7 +44,7 @@ blob_fixups: blob_fixups_user_type = {
 }  # fmt: skip
 
 module = ExtractUtilsModule(
-    'miuicamera-cupid',
+    'miuicamera-garnet',
     'xiaomi',
     blob_fixups=blob_fixups,
     lib_fixups=lib_fixups,

ZetyaWyn/device_xiaomi_garnet-miuicamera

BoardConfig.mk
@@ -5,9 +5,9 @@
 #
 
 # Inherit from the proprietary version
-include vendor/xiaomi/miuicamera-cupid/BoardConfigVendor.mk
+include vendor/xiaomi/garnet-miuicamera/BoardConfigVendor.mk
 
-MIUICAMERA_PATH := device/xiaomi/miuicamera-cupid
+MIUICAMERA_PATH := device/xiaomi/garnet-miuicamera
 
 # Properties
 TARGET_SYSTEM_PROP += $(MIUICAMERA_PATH)/system.prop

device.mk
@@ -5,7 +5,7 @@
 #
 
 # Inherit from the proprietary version
-$(call inherit-product, vendor/xiaomi/miuicamera-cupid/miuicamera-cupid-vendor.mk)
+$(call inherit-product, vendor/xiaomi/garnet-miuicamera/garnet-miuicamera-vendor.mk)
 
 # Public libraries
 PRODUCT_COPY_FILES += \

extract-files.py
@@ -18,7 +18,7 @@ from extract_utils.main import (
 )
 
 namespace_imports = [
-    'device/xiaomi/miuicamera-cupid',
+    'device/xiaomi/garnet-miuicamera',
 ]
 
 
@@ -44,7 +44,7 @@ blob_fixups: blob_fixups_user_type = {
 }  # fmt: skip
 
 module = ExtractUtilsModule(
-    'miuicamera-cupid',
+    'garnet-miuicamera',
     'xiaomi',
     blob_fixups=blob_fixups,
     lib_fixups=lib_fixups,
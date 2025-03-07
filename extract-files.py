f8b01f3c68b0e668028ac885b6c06325028e132e
kleidione Freitas <kleidione@gmail.com>
2025-03-07 18:48:49 -0300

    Switching to Garnet
    Signed-off-by: Kleidione Freitas <kleidione@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -18,7 +18,7 @@ from extract_utils.main import (
 )
 
 namespace_imports = [
-    'device/xiaomi/miuicamera-cupid',
+    'device/xiaomi/garnet-miuicamera',
 ]
 
 
@@ -44,7 +44,7 @@ blob_fixups: blob_fixups_user_type = {
 }  # fmt: skip
 
 module = ExtractUtilsModule(
-    'miuicamera-cupid',
+    'garnet-miuicamera',
     'xiaomi',
     blob_fixups=blob_fixups,
     lib_fixups=lib_fixups,
